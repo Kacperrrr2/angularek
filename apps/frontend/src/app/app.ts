cc3ab3aefdbd62691005c729c49448c51aced1c7
Kacper <k4cper.petelicki@gmail.com>
2026-07-03 14:31:23 +0200

    Add Tailwind CSS v4 to frontend via PostCSS

--- apps/frontend/src/app/app.ts
+++ apps/frontend/src/app/app.ts
@@ -1,9 +1,8 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
-import { NxWelcome } from './nx-welcome';
-
+import { Header } from './component/header/header';
 @Component({
-  imports: [NxWelcome, RouterModule],
+  imports: [RouterModule, Header],
   selector: 'app-root',
   templateUrl: './app.html',
   styleUrl: './app.css',
