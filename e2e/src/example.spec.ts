3834830b1072235e750ffd680adf0b8bf83e4965
Kacper <k4cper.petelicki@gmail.com>
2026-07-03 13:13:03 +0200

    feat: inital correct state to project

--- e2e/src/example.spec.ts
+++
@@ -1,8 +0,0 @@
-import { test, expect } from '@playwright/test';
-
-test('has title', async ({ page }) => {
-  await page.goto('/');
-
-  // Expect the page to have the correct title.
-  await expect(page).toHaveTitle('angularek');
-});
